a1ce6196ad21f5b4de001a46ee6b2148bf98f377
Angel Portal <obregon_portal@outlook.com>
2023-02-08 10:29:32 -0500

    Add the functions for Buttons

--- Calculadora/scrpit.js
+++ Calculadora/scrpit.js
@@ -1,28 +1,63 @@
+//write the element in the label
 function write(text){
     document.getElementById("TextArea").innerHTML += text;
 }
+//Replace all the text  in the labal 
+function Replace(text){
+    document.getElementById("TextArea").innerHTML = text;
+}
+
+//Read the the las element of the label
 function ReadElement (string, index = string.length - 1){
     return(string.slice(index))
 }
+
+//Find the last caracter diferent of the number in the label
 function FindLastOperator(string, char){
     const value = (Array.prototype.findLast.call(string, (c) => !(c <='9' && c >= '0'))); // Find the last operator diferent to number
     return value === char; 
 }
 
-function VerificationOperators(string){
+//Check if the operator is valided for write
+function VerificationOperators(string, val){
     const last = ReadElement(string);
 
     if (last <= '9' && last >= '0' ){
         
-        
-        if(val == '.' && FindLastOperator(text, '.')){
-
-        }else{
-            return true
+        if(val == '.' && FindLastOperator(string, '.')){
+            return false;
         }
+
+        return true;
     }
+
     return false;
 }
+
+//Makes the change of the special Butoons 
+function SpecialButoons (text, val){
+
+    switch (val) {
+        case 'delete':
+            text = " ";
+        break;
+        case 'back': 
+            text = text.slice(0, text.length -1 );
+        break;
+        case 'history':
+            //In process
+        break;
+        case 'solve':
+            text = eval(text)
+        break;   
+    }
+
+ 
+    Replace(text);
+
+}
+
+
 
 
 document.querySelectorAll('button').forEach(button => {
@@ -35,11 +70,11 @@ document.querySelectorAll('button').forEach(button => {
             break;
                  
             case 'ButtonOperation':
-                if(VerificationOperators(text)){write(val);}
+                if(VerificationOperators(text, val)){write(val);}
             break;
 
             case 'ButtonEspecial':
-                
+                SpecialButoons(text,val);
             break;
 
             default: 
